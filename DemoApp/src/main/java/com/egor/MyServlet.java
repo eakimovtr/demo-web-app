beb584e61ddc5a6f5386439da1371fb5cb40d6e8
Egor Akimov <akimoved@gmail.com>
2024-03-18 15:57:49 +0400

    add servlet context and config

---
+++ DemoApp/src/main/java/com/egor/MyServlet.java
@@ -0,0 +1,32 @@
+package com.egor;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		ServletContext ctx = getServletContext();
+		String contextStr = ctx.getInitParameter("Name");
+		out.println("Hi context " +contextStr);
+		
+		ServletConfig cfg = getServletConfig();
+		String configStr = cfg.getInitParameter("Name");
+		out.println("Hi config " +configStr);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
